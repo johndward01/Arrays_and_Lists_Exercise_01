639da4f82a802d16dee432ecd4c10632f6f0455b
johndward01 <johndward01@gmail.com>
2023-06-01 11:14:33 -0500

    update

--- Arrays_and_Lists_Exercise_01/Methods.cs
+++ Arrays_and_Lists_Exercise_01/Methods.cs
@@ -1,37 +1,71 @@
 ﻿namespace Arrays_and_Lists_Exercise_01;
 
-internal static class Methods
+public static class Methods
 {
-    // TODO: Create a method that takes an array of numbers and removes all of the even numbers
-    public static int[] RemoveEvens(int[] numbers)
+    // TODO: Create a method that takes an array of numbers and replaces all of the even numbers with 0
+    // and display them to the console
+    public static int[] ReplaceEvens(int[] numbers)
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                numbers[i] = 0;
+            }
+        }
+        return numbers;
     }
 
+
     // TODO: Create a method that takes an array of strings and removes all of the !'s
+    // and display them to the console
+    public static string[] RemoveExclamationMarks(string[] strs)
+    {
+        var newArray = new string[strs.Length];
+        var strWithoutExclamations = "";
+        for (int i = 0; i < strs.Length; i++)
+        {
+            for (int j = 0; j < strs[i].Length; j++)
+            {
+                if (strs[i][j] == '!')
+                {
+                    continue;
+                }
+                strWithoutExclamations += strs[i][j];                
+            }
+            newArray[i] = strWithoutExclamations;
+        }
+        return newArray;
+    }
 
 
     // TODO: Create a method that takes an array of Cats and sorts them by the Name's length (smallest first)
+    // and display them to the console
 
 
     // TODO: Create a method that takes an array of Dogs and sorts them by Breed (oldest first)
+    // and display them to the console
 
 
     // TODO: Create a method that takes an array of Animals and returns the sum of all the animals ages
+    // and display them to the console
 
 
     // TODO: Create a method that takes a list of numbers and removes any that are multiples of 3
+    // and display them to the console
 
 
     // TODO: Create a method that takes a list of bools and adds them together (true = 10, false = 0)
+    // and display them to the console
 
 
     // TODO: Create a method that takes a list of Dogs and a list of Cats and returns a list of Animals
     // that only has Cats OR Dogs with Ages above 10
+    // and display them to the console
 
 
     // TODO: Create a method that takes a list of IDbConnections and orders them by their ConnectionTimeout 
-
+    // and display them to the console
 }
 
 class Animal
--- Arrays_and_Lists_Exercise_01/Program.cs
+++ Arrays_and_Lists_Exercise_01/Program.cs
@@ -5,23 +5,35 @@ using System.Data;
 // Complete all of the TODO's here AND in the Methods class
 
 
-// TODO: Call the method that takes an array of numbers and removes all of the even numbers
+// TODO: Call the method that takes an array of numbers and replaces all of the even numbers with 0
+// and display them to the console
 int[] myIntArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-
+foreach (int i in Methods.ReplaceEvens(myIntArray))
+{
+   // Console.WriteLine(i);
+}
 
 // TODO: Call the method that takes an array of strings and removes all of the !'s
-string[] myStringArray = { "Zero", "On!e", "!Two", "Three!!", "!F!o!u!r!", "!!!!!Five", "Six", "!S!!e!!!v!!!!e!!!!!n!!!!!!", "!" };
-
+// and display them to the console
+string[] myStringArray = { "Zero", "On!e", "!Two", "Three!!", "!F!o!u!r!", "!!!!!Five", "Six", "!S!!e!!!v!!!!e!!!!!n!!!!!!", "Eight" };
+var result = Methods.RemoveExclamationMarks(myStringArray);
+foreach (string str in result)
+{
+    Console.WriteLine(str);
+}
 
 // TODO: Call the method that takes an array of Cats and sorts them by the Name's length (smallest first)
+// and display them to the console
 string[] catNames = { "Fluffy", "Whiskers", "Mittens", "Shadow", "Simba", "Milo", "Tiger", "Smoky", "Socks", "TabbyTabs" };
 
 
 // TODO: Call the method that takes an array of Dogs and sorts them by Breed (oldest first)
+// and display them to the console
 string[] dogNames = { "Max", "Bella", "Charlie", "Scrappy", "Scooby", "Rin Tin Tin", "Buddy", "Old Yeller", "Otis", "Bear" };
 
 
 // TODO: Call the method that takes an array of Animals and returns the sum of all the animals ages
+// and display them to the console
 #region Dogs and Cat (instantiated)
 var cat1 = new Cat() { Age = 10 };
 var cat2 = new Cat() { Age = 1 };
@@ -38,15 +50,18 @@ var animals = new Animal[] { cat1, cat2, cat3, cat4, cat5, dog1, dog2, dog3, dog
 
 
 // TODO: Call the method that takes a list of numbers and removes any that are multiples of 3
+// and display them to the console
 List<int> numbers = new List<int> { 45, 12, 67, 33, 88, 10, 23, 90, 51, 78 };
 
 
 // TODO: Call the method that takes a list of bools and adds them together (true = 10, false = 0)
+// and display them to the console
 List<bool> boolList = new List<bool> { true, false, true, true, false, true, false, false, true, true };
 
 
 // TODO: Call the method that takes a list of Dogs and a list of Cats and returns a list of Animals
 // that only has Cats OR Dogs with Ages above 10
+// and display them to the console
 #region Dogs and Cat (instantiated)
 var cat10 = new Cat() { Age = 10 };
 var cat20 = new Cat() { Age = 11 };
@@ -64,6 +79,7 @@ var cats = new List<Cat>() { cat10, cat20, cat30, cat40, cat50 };
 
 
 // TODO: Call the method that takes a list of IDbConnections and orders them by their ConnectionTimeout 
+// and display them to the console
 #region Connections (instantiated)
 var mySql = new MySQLConnection();
 var postgreSql = new PostgreSQLConnection();
